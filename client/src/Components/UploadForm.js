887092d69c00e678c99f29511f897415db7c4f1d
Brent Abruzese <brent.abruzese@gmail.com>
2020-01-09 11:14:23 -0500

    changed page layouts

--- client/src/Components/UploadForm.js
+++ client/src/Components/UploadForm.js
@@ -1,27 +1,54 @@
 import React, { useState } from 'react';
-import { Container, Row, Button, Form, Col } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
+import axios from 'axios'
 
 function UploadForm() {
-  const [file, setFile] = useState('')
-  
+  const [file, setFile] = useState(null)
+  const [filename, setFileName] = useState('choose file')
+  const [uploadedFile, setUploadedFile] = useState({ filename: null, filepath: null })
+
+  const onChange = e => {
+    setFile(e.target.files[0])
+    setFileName(e.target.files[0].name)
+  }
+
+  const onSubmit = async e => {
+    e.preventDefault()
+    const formData = new FormData()
+    formData.append('file', file)
+
+    try {
+      const res = await axios.post(`/upload`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+      })
+      const { fileName, filePath } = res.data
+      setUploadedFile({ filename: fileName, filepath: filePath })
+    }
+    
+    catch(error) {
+      if(error) {
+        console.log('there was a problem with the server')
+      } else {
+        console.log(error)
+      }
+    }
+  }
+
   return (
     <>
       <Container>
         <Row>
-          <Col xs={6}>
-            <Form>
-            <Form.Row>
-            <Form.Group controlId="formBasicSubmit">
-              <Form.Label>Import your DB</Form.Label>
-              <Form.Control type="textarea" />
-                <Form.Control type="file" variant="outline-primary" accept=".csv"/>
-                <Button variant="outline-secondary">Import</Button>
-            </Form.Group>
-            </Form.Row>
-            </Form>
+          <Col sm={3}> 
+            <form onSubmit={onSubmit}>
+              <input type="file" onChange={onChange} id="fileUpload" name="file" variant="outline-primary" accept=".csv"/>
+              <input type="submit" value="Upload!" />
+            </form>
           </Col>
         </Row>
       </Container>
+      { uploadedFile ? filename : 'no files uploaded..' }
     </>
   )
 }
